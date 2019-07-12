d48cd64d16d1bd7971380bb820ae1c844bfeb3ef
Adam Johanides <johanides.adam@gmail.com>
2019-07-12 10:32:50 +0200

    Merge branch 'master' of /home/johanada/Gecco/S3FakeNews with conflicts.

--- scripts/inference_keras.py
+++ scripts/inference_keras.py
@@ -1,10 +1,5 @@
-import read_csv
-import doc2vec_test
-from keras.models import Sequential
-from keras.layers import Dense
-
-import numpy as np
-from tqdm import tqdm
+import scripts.read_csv as read_csv
+import scripts.doc2vec_test as doc2vec_test
 
 
 dataset = read_csv.return_dataset()
@@ -15,43 +10,3 @@ doc2vec_transformer = doc2vec_test.doc2vec_converter_object()
 index = 5
 print("vector",doc2vec_transformer.trans(dataset[index][0]))
 print("class", dataset[index][1])
-
-
-## Model ##
-
-model = Sequential()
-model.add(Dense(units=7, activation='tanh', input_dim=10))
-model.add(Dense(units=5, activation='tanh'))
-model.add(Dense(units=3, activation='tanh'))
-model.add(Dense(units=1, activation='sigmoid'))
-
-
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-
-
-N = 20761
-train_perc = 0.75
-
-print("transformig with doc2vec")
-x_train = np.array([doc2vec_transformer.trans(dataset[index][0]) for index in tqdm(range(int(N * train_perc)))])
-y_train = np.array([dataset[index][1] for index in range(int(N*train_perc))])
-
-
-
-model.fit(x_train, y_train, epochs=15, batch_size=32)
-
-
-print("generating test set")
-x_test = np.array([doc2vec_transformer.trans(dataset[index][0]) for index in tqdm(range(int(N * train_perc),N))])
-y_test = np.array(np.array([dataset[index][1] for index in range(int(N*train_perc),N)]))
-
-classes = model.predict(x_test, batch_size=128)
-
-
-classes = np.around(classes, 0)
-
-print("error perc: ",100*np.average(np.absolute(classes - y_test)))
-print(classes)
